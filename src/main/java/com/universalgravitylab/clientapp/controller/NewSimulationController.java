f79b43370325c80245cf4cbeabb082f69e598e14
Aliaksei Papou <weatherlet@gmail.com>
2023-07-30 22:46:24 +0300

    Added Cell Value Factories to Table columns

--- src/main/java/com/universalgravitylab/clientapp/controller/NewSimulationController.java
+++ src/main/java/com/universalgravitylab/clientapp/controller/NewSimulationController.java
@@ -4,17 +4,25 @@ import com.universalgravitylab.clientapp.Closable;
 import com.universalgravitylab.clientapp.MainController;
 import com.universalgravitylab.clientapp.model.Body;
 import com.universalgravitylab.clientapp.model.Simulation;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
 @Component
-public class NewSimulationController implements Closable {
+public class NewSimulationController implements Closable, Initializable {
 
     @Autowired
     private MainController mainController;
@@ -22,6 +30,21 @@ public class NewSimulationController implements Closable {
 
     @FXML
     private TableView<Body> bodyTable;
+
+    @FXML
+    private TableColumn<Body, String> nameColumn;
+
+    @FXML
+    private TableColumn<Body, Double> massColumn;
+
+    @FXML
+    private TableColumn<Body, Color> colorColumn;
+
+    @FXML
+    private TableColumn<Body, String> positionColumn;
+
+    @FXML
+    private TableColumn<Body, String> velocityColumn;
 
     @Override
     public void onClose(Event event) {
@@ -44,8 +67,14 @@ public class NewSimulationController implements Closable {
 
     public void updateUI() {
         List<Body> bodyList = simulation.getBodyList();
-        for (Body body: bodyList) {
-            bodyTable.getItems().add(body);
-        }
+        ObservableList<Body> bodies = FXCollections.observableArrayList(bodyList);
+        bodyTable.setItems(bodies);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Body, String>("name"));
+        massColumn.setCellValueFactory(new PropertyValueFactory<Body, Double>("mass"));
+        colorColumn.setCellValueFactory(new PropertyValueFactory<Body, Color>("color"));
     }
 }
